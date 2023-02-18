e6f9fc198e58ebe08a7a10a07cda88cf19127765
Bobby Luly <bobbyluly@gmail.com>
2023-02-18 11:23:21 -0700

    Pass story 2, need to add model test for #average_age

--- app/controllers/astronauts_controller.rb
+++ app/controllers/astronauts_controller.rb
@@ -1,5 +1,6 @@
 class AstronautsController < ApplicationController
   def index
     @astronauts = Astronaut.all
+    @astro_avg_age = Astronaut.average_age
   end
 end
--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -9,12 +9,25 @@ RSpec.describe 'Astronaut index page', type: :feature do
       it 'I see a list of astronauts and their attribures' do
         visit '/astronauts'
         save_and_open_page
-        expect(page).to have_content("Name: #{astronaut_1.name}")
-        expect(page).to have_content("Age: #{astronaut_1.age}")
-        expect(page).to have_content("Job: #{astronaut_1.job}")
-        expect(page).to have_content("Name: #{astronaut_2.name}")
-        expect(page).to have_content("Age: #{astronaut_2.age}")
-        expect(page).to have_content("Job: #{astronaut_2.job}")
+
+        expect(page).to have_content("All Astronauts")
+        within "div#astronaut_info" do
+          expect(page).to have_content("Name: #{astronaut_1.name}")
+          expect(page).to have_content("Age: #{astronaut_1.age}")
+          expect(page).to have_content("Job: #{astronaut_1.job}")
+          expect(page).to have_content("Name: #{astronaut_2.name}")
+          expect(page).to have_content("Age: #{astronaut_2.age}")
+          expect(page).to have_content("Job: #{astronaut_2.job}")
+        end
+      end
+
+      it 'I see the average age of all astronauts' do
+        astronaut_3 = Astronaut.create!(name: 'Neil Legstrong', age: 40, job: 'Aging Space Pirate')
+
+        visit '/astronauts'
+        save_and_open_page
+
+        expect(page).to have_content("Average Age: 62.33")
       end
     end
   end
